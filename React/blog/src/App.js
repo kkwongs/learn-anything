bf302e813266cebcc0d8f833cfb5264763053c98
Gihyun Gwon <kihyun5741@gmail.com>
2024-09-22 14:28:59 +0900

    React - state(숙제)

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -4,17 +4,19 @@ import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
-  let [글제목, b] = useState("여자 코트 추천");
+  let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className="list">
-        <h4>{글제목}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {글제목.map((title, index) => (
+        <div className="list" key={index}>
+          <h4>{title}</h4>
+          <p>2월 17일 발행</p>
+        </div>
+      ))}
     </div>
   );
 }
