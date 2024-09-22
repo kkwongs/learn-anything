4861ec27587e712aed7cafad13fb004040983fcc
Gihyun Gwon <kihyun5741@gmail.com>
2024-09-22 15:13:10 +0900

    React - state 변경 방법

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
@@ -5,6 +7,11 @@ import { useState } from "react";
 function App() {
   let post = "강남 우동 맛집";
   let [글제목, b] = useState(["남자코트 추천", "강남 우동맛집", "파이썬독학"]);
+  let [likeCount, setLikeCount] = useState(0);
+
+  function upLikeCount() {
+    setLikeCount(likeCount + 1);
+  }
 
   return (
     <div className="App">
@@ -13,7 +20,9 @@ function App() {
       </div>
       {글제목.map((title, index) => (
         <div className="list" key={index}>
-          <h4>{title}</h4>
+          <h4>
+            {title} <span onClick={upLikeCount}>👍</span> {likeCount}
+          </h4>
           <p>2월 17일 발행</p>
         </div>
       ))}
